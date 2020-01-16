3b87514984ce1ee2ce1a2d3b2896188d89896ba3
Student 236597 <236597@student.pwr.edu.pl>
2020-01-16 10:47:14 +0100

    All models necessary for presentation

--- APP/WebApplication/Models/Basket.cs
+++ APP/WebApplication/Models/Basket.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,13 @@ namespace WebApplication.Models
 {
     public class Basket
     {
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        [Range(1, 999)]
+        public int AmountInBasket { get; set; }
     }
 }
