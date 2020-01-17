6472b8ca35a4d7987ecc6ed35caf0093195ad317
Student 236597 <236597@student.pwr.edu.pl>
2020-01-17 15:23:57 +0100

    Initial commit

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using ShopLogin.Models;
+
+namespace WebApplication.Controllers
+{
+    public class CustomersController : Controller
+    {
+
+        // GET: Customers
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ActionResult Registration()
+        {
+            
+            return View(new Customer());
+        }
+
+        public ActionResult Index()
+        {
+            var customers = _context.Customers.ToList();
+            return View(customers);
+        }
+
+        [HttpPost]
+        public ActionResult NewCustomer(Customer customer, Address address)
+        {
+            _context.Addresses.Add(address);
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Models/Address.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class Address
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(60)]
+        public string City { get; set; }
+
+        [StringLength(60)]
+        public string Street { get; set; }
+
+        [StringLength(10)]
+        public string Building { get; set; }
+
+        [StringLength(10)]
+        public string Apartment { get; set; }
+
+        [StringLength(10)]
+        public string PostCode { get; set; }
+
+    }
+}
