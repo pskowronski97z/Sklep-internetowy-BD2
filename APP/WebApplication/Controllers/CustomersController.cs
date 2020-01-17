09208369899f1d82aaa389ee980fa7d59a95a2ff
Student 236597 <236597@student.pwr.edu.pl>
2020-01-17 12:31:57 +0100

    Start working 17.01

--- APP/WebApplication/Controllers/CustomersController.cs
+++ APP/WebApplication/Controllers/CustomersController.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebApplication.Models;
+using WebApplication.ViewModels;
 using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
 
 namespace WebApplication.Controllers
 {
     public class CustomersController : Controller
     {
+        private Microsoft.AspNet.Identity.UserManager<IdentityUser> userManager;
+        private SignInManager<IdentityUser> signInManager;
+
+        public CustomersController(Microsoft.AspNet.Identity.UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
         // GET: Customers
         private DataContext _context;
 
@@ -29,9 +44,27 @@ namespace WebApplication.Controllers
             return View(new Customer());
         }
 
+        [HttpGet]
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Login, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index","Home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Invali Login Attempt");
+            }
+            return View(model);
         }
 
         public ActionResult Index()
---
+++ APP/WebApplication/ViewModels/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Login { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Zapamiętaj mnie")]
+        public bool RememberMe { get; set; }
+    }
+}
